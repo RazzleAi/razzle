09a856e4a5c9ac0c53e5df36c01e9d25639046bf
tomi <tomialagbe@assetreality.com>
2023-05-22 18:00:09 +0300

    WIP. Python SDK

--- libs/python-sdk/example/main.py
+++ libs/python-sdk/example/main.py
@@ -1,4 +1,5 @@
 from razzlesdk import Razzle, RazzleProps, Action, ActionParam
+import os
 
 class HelloApp(object):
 
@@ -8,7 +9,14 @@ class HelloApp(object):
 
 if __name__ == "__main__":
     app = HelloApp()
-    appId = ""
-    apiKey = ""
+    appId = "38923f681c6acf00e79bdba06631eaf9"
+    apiKey = "e331473e3f804b0a3a2a573422eb5b4cc36a8effd94a31e8fcff8b17b540eddf"
     
     razzleApp = Razzle(RazzleProps(appId, apiKey, False, [], app.handlePrompt))
+
+    while True:
+        inp = input("Press q to quit: ")
+        if inp == "q":
+            break
+
+    os._exit(0)
