e0f34cf79752e863d7b848df86348c0db0375b24
Olabode <olabode.eesti@gmail.com>
2023-06-13 17:19:20 +0300

    Added docs project

---
+++ apps/docs/app/test-component.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export function TestComponent() {
+  return (
+    <div>
+      <h1>Test Component</h1>
+      <p>This is a test component</p>
+    </div>
+  )
+}
---
+++ apps/docs/next.config.js
@@ -0,0 +1,38 @@
+//@ts-check
+
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    providerImportSource: "@mdx-js/react",
+  },
+})
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { composePlugins, withNx } = require('@nx/next')
+
+/**
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  nx: {
+    // Set this to true if you would like to use SVGR
+    // See: https://github.com/gregberge/svgr
+    svgr: false,
+  },
+}
+
+const plugins = [
+  // Add more Next.js plugins to this list if needed.
+  withNx
+]
+
+module.exports = composePlugins(...plugins)(nextConfig)
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+})
